f1f3676813245667b60b21918fd72919eb16b41b
Eisuke Esaki <izkyyy@gmail.com>
2022-05-13 13:56:02 -0700

    removed unused params from controller funcs

--- UserController.js
+++ UserController.js
@@ -2,7 +2,7 @@ const db = require("./db");
 
 module.exports = {
 
-  getAllUsers(req, res, next) {
+  getAllUsers(req, res) {
     const users = db.Table_Users;
     if (!users) {
       return res.status(404).send({ message: "users not found" });
@@ -10,7 +10,7 @@ module.exports = {
     res.json(users);
   },
 
-  getUserById(req, res, next) {
+  getUserById(req, res) {
     const user = db.Table_Users[req.params.id];
     if (!user) {
       return res.status(404).send({ message: "user not found" });
@@ -18,7 +18,7 @@ module.exports = {
     res.json(user);
   },
 
-  createUser(req, res, next) {
+  createUser(req, res) {
     const idNewUser = db.Table_Users.length;
     const newUser = {
       id: idNewUser,
@@ -28,7 +28,7 @@ module.exports = {
     res.json({ message: "CREATEd new user", user: db.Table_Users[idNewUser] });
   },
 
-  deleteUserById(req, res, next) {
+  deleteUserById(req, res) {
     const idUserDel = req.params.id;
     if (!db.Table_Users[idUserDel]) {
       return res.status(400).send("requested user does not match any of the records in db");
@@ -37,7 +37,7 @@ module.exports = {
     res.json({ message: `DELETED user with an id of ${idUserDel}` });
   },
 
-  updateUserById(req, res, next) {
+  updateUserById(req, res) {
     const idUserUpd = req.params.id;
     if (!db.Table_Users[idUserUpd]) {
       return res.status(400).send("requested user does not match any of the records in db");
