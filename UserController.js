1837463fab59ef1883c9b9cd676492dbcbb0546c
Eisuke Esaki <izkyyy@gmail.com>
2022-05-13 12:45:36 -0700

    POST /users

--- UserController.js
+++ UserController.js
@@ -13,10 +13,31 @@ module.exports = {
   getUserById(req, res, next) {
     const user = db.Table_Users[req.params.id];
     if (!user) {
-      return res.status(404).send({ message: "user not found"});
+      return res.status(404).send({ message: "user not found" });
     }
     res.json(user);
+  },
+
+  createUser(req, res, next) {
+    const idNewUser = db.Table_Users.length;
+    const newUser = {
+      id: idNewUser,
+      username: req.body.username
+    };
+    db.Table_Users.push(newUser);
+    res.json({ message: "created new user", user: db.Table_Users[idNewUser] });
   }
 
 }
+
+/*
+    res.send([body])
+        send HTTP response
+        body = Buffer || String || object || Boolean || Array
+        automatically assigns Content-Length
+
+    res.json([body])
+        send JSON response
+        body = any JSON type
+*/
 
--- routes.js
+++ routes.js
@@ -5,5 +5,6 @@ const UserController = require("./UserController");
 
 routes.get("/users", UserController.getAllUsers);
 routes.get("/users/:id", UserController.getUserById);
+routes.post("/users", UserController.createUser);
 
 module.exports = routes;
