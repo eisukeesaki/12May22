ad79faf87c2521fcdb5ab5a5b9d00077f2a5da52
Eisuke Esaki <izkyyy@gmail.com>
2022-05-12 21:15:40 -0700

    added error handling: getAllUsers, getUserById

--- UserController.js
+++ UserController.js
@@ -3,11 +3,19 @@ const db = require("./db");
 module.exports = {
 
   getAllUsers(req, res, next) {
-    res.json(db.Table_Users);
+    const users = db.Table_Users;
+    if (!users) {
+      return res.status(404).send({ message: "users not found" });
+    }
+    res.json(users);
   },
 
   getUserById(req, res, next) {
-    res.json(db.Table_Users[req.params.id]);
+    const user = db.Table_Users[req.params.id];
+    if (!user) {
+      return res.status(404).send({ message: "user not found"});
+    }
+    res.json(user);
   }
 
 }
